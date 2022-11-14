14db0af5ff37d3b21c2ee429e77b05f80f1b99ee
Abhimanyu Yadav <abhi12manyu@gmail.com>
2022-11-14 18:06:11 +0000

    functionaV2

--- reserve.js
+++ reserve.js
@@ -35,3 +35,22 @@ function validateEmail(input, requiredMsg, invalidMsg) {
 	}
 	return true;
 }
+
+const form = document.querySelector("#signup");
+
+const NAME_REQUIRED = "Please enter your name";
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter a correct email address format";
+
+form.addEventListener("submit", function (event) {
+	// stop form submission
+	event.preventDefault();
+
+	// validate the form
+	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+	// if valid, submit the form.
+	if (nameValid && emailValid) {
+		alert("Demo only. No form was posted.");
+	}
+});
