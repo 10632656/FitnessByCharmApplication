ba1a9bcdd8427bb3f1708de88d9dab5ce08e4168
Marouan <115103579+marouanal@users.noreply.github.com>
2022-11-22 10:07:13 +0000

    Add files via upload

---
+++ FitCorner/script.js
@@ -0,0 +1,41 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+let http = new XMLHttpRequest();
+http.open('get', 'products.json', true);
+http.send();
+http.onload = function(){
+   if(this.readyState == 4 && this.status == 200){
+      let products = JSON.parse(this.responseText);
+      let output = "";
+      for(let item of products){
+         output += `
+            <div class="product">
+               <img src="${item.image}" alt="${item.description}">
+               <p class="title">${item.title}</p>
+               <p class="description">${item.description}</p>
+               <p class="price">
+                  <span>${item.price}</span>
+                  <span>€</span>
+               </p>
+               <p class="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
+            </div>
+         `;
+      }
+      document.querySelector(".products").innerHTML = output;
+   }
+
+   
+}
