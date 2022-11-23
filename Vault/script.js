5b1d78e0626f23658c9d9f505e3266084ecd6866
abhi12manyu <abhi12manyu@gmail.com>
2022-11-23 00:52:29 +0000

    script fixes

--- Vault/script.js
+++ Vault/script.js
@@ -13,3 +13,48 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+let http = new XMLHttpRequest();
+http.open('get', 'products.json', true);
+http.send();
+http.onload = function(){
+   if(this.readyState == 4 && this.status == 200){
+      let products = JSON.parse(this.responseText);
+      let output = "";
+      for(let item of products){
+         output += `
+            <div class="product">
+               <img src="${item.image}" alt="${item.description}">
+               <p class="title">${item.title}</p>
+               <p class="description">${item.description}</p>
+               <p class="price">
+                  <span>${item.price}</span>
+                  <span>€</span>
+               </p>
+               <p class="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
+            </div>
+         `;
+      }
+      //document.querySelector(".products").innerHTML = output;
+   }
+
+   
+}
+
+function validateform() {
+   if (!ValidateEmail()){
+      alert('Please enter valid mail');
+    }    
+    else{
+         alert("Thank you! Your item has been reserved.")
+        }
+}
+function ValidateEmail() {
+   var vmail = $('#E-mail').val();   
+   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   if(vmail.match(mailformat)){
+     return true;
+   }
+   else{
+     return false;
+   }
+}
